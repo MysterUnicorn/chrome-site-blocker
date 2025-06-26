b2efbb4bd5a3b2000c2dedb43b32ce9f2af48e3a
Richard Gui <richard.gui@tmrw.com>
2025-06-26 17:39:24 +0200

    refactor options.js and extend options.html with cover elements table.

--- options.js
+++ options.js
@@ -1,63 +1,66 @@
-const getRules = chrome.declarativeNetRequest.getDynamicRules;
-const updateRules = chrome.declarativeNetRequest.updateDynamicRules;
+class BlockedSitesTable {
+  constructor(tableId, inputId, appendRowButtonId) {
+    this.table = document.getElementById(tableId);
+    this.input = document.getElementById(inputId);
+    this.appendRowButton = document.getElementById(appendRowButtonId);
+    this.getRules = chrome.declarativeNetRequest.getDynamicRules;
+    this.updateRules = chrome.declarativeNetRequest.updateDynamicRules;
+    this.init();
+  }
 
+  async appendRow() {
+    const newEntry = this.input.value;
+    this.input.value = "";
+    const rules = await this.getRules();
+    const newId = (Math.max(...(rules.map((rule) => rule["id"])), 0)) + 1;
+    await this.updateRules({
+      addRules: [{
+        id: newId,
+        priority: 1,
+        action: { type: "redirect", redirect: { url: "https://google.com" } },
+        condition: { urlFilter: `||${newEntry}`, resourceTypes: ["main_frame"] }
+      }]
+    });
+    await this.reloadTable();
+  }
 
-async function addNewRow() {
-  const newEntry = document.getElementById("new-entry").value
-  document.getElementById("new-entry").value = ""
-  rules = await getRules()
-  newId = (Math.max( ...(rules.map((rule) => {return rule["id"]})), 0 ))+ 1
-  console.log(newId)
-  console.log(typeof(newId))
-  await updateRules({addRules: [{
-    "id": newId,
-    "priority": 1,
-    "action": { "type": "redirect", "redirect": { "url": "https://google.com" } },
-    "condition": { "urlFilter": `||${newEntry}`, "resourceTypes": ["main_frame"] }
-  }]})
-  await fillTable();
-}
+  async removeRow(id) {
+    await this.updateRules({ removeRuleIds: [id] });
+    await this.reloadTable();
+  }
 
-async function removeRow(id) {
-  updateRules({removeRuleIds: [id]})
-  fillTable()
-}
+  createRowHTTPTemplate(id, site) {
+    var template = document.createElement('template');
+    template.innerHTML = `<tr><th scope="row">${id}</th><td>${site}</td><td><button id="removeRow${id}">Remove</button></td></tr>`;
+    return template.content.childNodes[0];
+  }
 
-function templateRow(id, site) {
-  var template = document.createElement('template');
-  console.log("a")
-  template.innerHTML = `<tr><th scope="row">${id}</th><td>${site}</td><td><button id="removeRow${id}">Remove</button></td></tr>`;
-  return template.content.childNodes[0]
-}
+  async reloadTable() {
+    const rules = await this.getRules();
+    this.table.innerHTML = "";
+    const sortedRules = rules.sort((rule1, rule2) => rule1["id"] - rule2["id"]);
+    sortedRules.forEach((rule) => {
+      var rowNode = this.createRowHTTPTemplate(rule["id"], rule["condition"]["urlFilter"].replace("||", ""));
+      this.table.appendChild(rowNode);
+      document.getElementById(`removeRow${rule["id"]}`).addEventListener("click", async () => {
+        await this.removeRow(rule["id"]);
+      });
+    });
+  }
 
-async function fillTable() {
-  const table = document.getElementById("to-block-site-list");
-  
-  const rules = await getRules()
-  table.innerHTML = ""
-  const sortedRules = rules.sort((rule1, rule2) => {return rule1["id"]-rule2["id"]})
-  sortedRules.forEach(
-    (rule) => {
-      var rowNode = templateRow(rule["id"], rule["condition"]["urlFilter"].replace("||", ""))
-      table.appendChild(rowNode);
-      document.getElementById(`removeRow${rule["id"]}`).addEventListener("click", async () =>{
-        await removeRow(rule["id"])
-      })
-    })
+  init() {
+    this.appendRowButton.addEventListener("click", async () => {
+      await this.appendRow();
+    });
+    this.input.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        this.appendRowButton.click();
+      }
+    });
+    this.reloadTable();
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const addButton = document.getElementById("addNewRow");
-    addButton.addEventListener("click", async () => {
-        await addNewRow();
-    });
-  });
-
-document.getElementById("new-entry").addEventListener('keypress', function(event) {
-    const addButton = document.getElementById("addNewRow");
-    if (event.key === 'Enter') {
-        addButton.click(); // This triggers the button
-    }
+  new BlockedSitesTable("to-block-site-list", "block-new-entry", "block-append-row");
 });
-
-fillTable()
